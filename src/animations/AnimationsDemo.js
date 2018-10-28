67ab4f7e6ee737419223fc6b7759bd2fde995643
Ecker <jim@ecker.com>
2018-10-28 15:23:29 -0400

    add 'add row' feature

--- src/animations/AnimationsDemo.js
+++ src/animations/AnimationsDemo.js
@@ -11,46 +11,80 @@ class Square extends React.Component {
     }
   }
  */
+
+ class FooWorld {
+   id: 0;
+   title = '';
+   score = 0;
+ }
   
   class AnimationsDemo extends React.Component {
     constructor(props) {
         super(props); 
-        this.state = { counter: 0 }
+        /*
+        const row1 = FooWorld();
+        row1.title = 'Foo World 123';
+        row1.score = 88;
+        row1.id = 999;
+        */
+        this.state = { 
+          counter: 0,
+          acctList: [{title: 'foo world A', score: 88, id: 999}]
+         }
         //this.handleClick = this.handleClick.bind(this);
     }
 
-    /*
-     handleClick() {
-        console.log('add row');
-      };
-      */
-     handleClick = () => {
-        console.log('add row');
+    handleClick = (event, index) => {
+        console.log('add row', event);
+        const newRow = {
+          title: 'Another row ' + index,
+          score: 99,
+          id: index + 1
+        }
         this.setState((prevState) => ({
-           counter: prevState.counter + 1
+           counter: prevState.counter + 1,
+           acctList: [...prevState.acctList, newRow] 
         }));
-      };
+    };
 
     render() {
+      console.log('render() starting');
       return (
-        <div className="inputRow">
-           <span className='item field1'>FooBar</span>
-           <span className='item field2'> test 123</span>
-           <span className='item btn'>
-             <button onClick={this.handleClick}> add2 </button>
-            </span>
-            <button onClick={this.handleClick}>
-           {this.state.counter}
-       </button>
-        </div>
-
-      );
+        <span>
+          {this.state.acctList.map((item, i) => {
+            return( 
+            <div key={i} className="inputRow" >
+               <span className='item field1'>{item.title}</span>
+               <span className='item field2'> test 123</span>
+               <span className='item btn'>
+                 <button onClick={this.handleClick}> add </button>
+                </span>
+                <button onClick={(e) => { this.clickMe(e, i) }} >
+                   {this.state.counter}
+                </button>
+            </div>
+              )
+          } )}
+        </span>
+        );
     }
   }
   
   // ========================================
   
   /*
+            <div key={i} className="inputRow" >
+               <span className='item field1'>{item.title}</span>
+               <span className='item field2'> test 123</span>
+               <span className='item btn'>
+                 <button onClick={this.handleClick}> add2 </button>
+                </span>
+                <button onClick={this.handleClick}>
+                   {this.state.counter}
+                </button>
+            </div>
+
+
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
